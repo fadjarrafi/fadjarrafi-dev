6f24d82841d5124bf4796a38bd0aae2ef12487d0
fadjarrafi <dark.fir21@gmail.com>
2023-12-31 16:27:00 +0700

    [fix] broken link in project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'Activation Function in Bi-Directional LSTM',
     description: `This is my bachelor's thesis, part of a collaborative research program with my lecturer. At the time, my university's computer science faculty was launching a collaborative research initiative to encourage students to do and develop research that would be applied as thesis material.`,
     imgSrc: '/static/images/bilstm.webp',
-    href: '/blog/activation-function-in-deep-learning',
+    href: 'https://fadjarrafi-dev.vercel.app/blog/deep-learning/activation-function-in-deep-learning',
   },
 ]
 
