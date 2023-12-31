16e3d2e43fa231d0e6ee2f4738fabb53a38fd679
fadjarrafi <dark.fir21@gmail.com>
2023-12-31 16:39:13 +0700

    [feat] (projects): update content in projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,21 @@
 const projectsData = [
   {
-    title: 'Activation Function in Bi-Directional LSTM',
-    description: `This is my bachelor's thesis, part of a collaborative research program with my lecturer. At the time, my university's computer science faculty was launching a collaborative research initiative to encourage students to do and develop research that would be applied as thesis material.`,
-    imgSrc: '/static/images/bilstm.webp',
-    href: 'https://fadjarrafi-dev.vercel.app/blog/deep-learning/activation-function-in-deep-learning',
+    title: 'Square Indonesia',
+    description: `Square Indonesia is a digital marketing agency on a mission to empower businesses with impactful solutions that drive tangible results. We believe in the power of integrity, goal-oriented strategies, and customer satisfaction to fuel success in the ever-evolving digital landscape.`,
+    imgSrc: '/static/images/square-indonesia.png',
+    href: 'https://square-indonesia.vercel.app/',
+  },
+  {
+    title: 'Szeto Accurate Consultants',
+    description: `Szeto Accurate Consultants (SAC) is the pioneer of the first accurate implementation service in Indonesia which has officially collaborated with CPSSoft (as the owner of Accurate Accounting Software) since April 2012.`,
+    imgSrc: '/static/images/szeto.png',
+    href: 'https://formonsite.szetoaccurate.com/',
+  },
+  {
+    title: 'Wira Manning Services',
+    description: `PT. Cipta Wira Tirta also known as Wira Manning Services was established in May 2000 as a representative office for Wilhelmsen Ship Management formerly known as Barber International. At the initial stage, the company served for a cruise line company based in Miami, USA.`,
+    imgSrc: '/static/images/ciptawiratirta2.png',
+    href: 'https://ciptawiratirta.com',
   },
 ]
 
