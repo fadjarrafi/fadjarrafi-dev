fcdeaa989421ca746d26a1fe115d86caaecd2ab1
Fadjar Irfan rafi <dark.fir21@gmail.com>
2024-06-16 20:19:29 +0700

    Fix header font Main page

--- app/Main.tsx
+++ app/Main.tsx
@@ -29,9 +29,9 @@ export default function Home({ posts }) {
       </div>
       <div className="mt-20 divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest Post
-          </h1>
+          </h2>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
@@ -53,14 +53,14 @@ export default function Home({ posts }) {
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h3 className="text-xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
                             >
                               {title}
                             </Link>
-                          </h2>
+                          </h3>
                           <div className="flex flex-wrap">
                             {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
--- data/snippetsData.ts
+++ data/snippetsData.ts
@@ -2,25 +2,7 @@ const snippetsData = [
   {
     title: 'SQL Join',
     description: `PT. Cipta Wira Tirta also known as Wira Manning Services was established in May 2000 as a representative.`,
-    imgSrc: '/static/images/bootstrap.svg',
-    href: 'https://square-indonesia.vercel.app/',
-  },
-  {
-    title: 'Szeto Accurate',
-    description: `PT. Cipta Wira Tirta also known as Wira Manning Services was established in May 2000 as a representative.`,
-    imgSrc: '/static/images/bootstrap.svg',
-    href: 'https://formonsite.szetoaccurate.com/',
-  },
-  {
-    title: 'Wira Manning Services',
-    description: `PT. Cipta Wira Tirta also known as Wira Manning Services was established in May 2000 as a representative.`,
-    imgSrc: '/static/images/bootstrap.svg',
-    href: 'https://ciptawiratirta.com',
-  },
-  {
-    title: 'Square Indonesia',
-    description: `PT. Cipta Wira Tirta also known as Wira Manning Services was established in May 2000 as a representative.`,
-    imgSrc: '/static/images/bootstrap.svg',
+    imgSrc: '/static/images/mysql.svg',
     href: 'https://square-indonesia.vercel.app/',
   },
 ]
